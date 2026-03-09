e59b7a6134d238dcfd1753073bab8e4d4be9f347
Germain Hirwa <higermain1@gmail.com>
2026-03-09 00:20:08 -0400

    fix: stripe api version typings

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-02-24.acacia',
+  apiVersion: '2026-02-25.clover',
   appInfo: {
     name: 'InterviewIQ Premium',
     version: '1.0.0',
